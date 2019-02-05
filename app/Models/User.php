cdc4c65baf8f208e96b1c433f6b1dd3eed382051
vagrant <vagrant@homestead>
2019-02-05 13:44:38 +0000

    List topics created by user

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,4 +26,11 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+
+    //关联话题模型
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
 }

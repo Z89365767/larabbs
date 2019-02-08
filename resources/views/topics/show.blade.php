0d9521842cd6d0852d7cc067e3ae25e1dfb437a7
vagrant <vagrant@homestead>
2019-02-08 13:24:44 +0000

    List user replies

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -66,6 +66,16 @@
                 @endcan
             </div>
         </div>
+
+        {{-- 用户回复列表 --}}
+        <div class="panel panel-default topic-reply">
+            <div class="panel-body">
+                @include('topics._reply_box',['topic' => $topic])
+                {{-- 注意读取回复列表时需使用懒加载来避免 N+1 问题 --}}
+                @include('topics._reply_list',['replies' => $topic->replies()->with('user')->get()])
+            </div>
+
+        </div>
     </div>
 </div>
 

60835e563479aa78c7d5dfdef176dfe154db42f3
vagrant <vagrant@homestead>
2019-02-11 06:07:54 +0000

    Admin can manage roles and permissions

--- config/administrator.php
+++ config/administrator.php
@@ -64,7 +64,12 @@ return array(
      *	)
      */
     'menu' => [
-        '用户与权限' => 'users',
+        '用户与权限' => [
+
+            'users',
+            'roles',
+            'permissions',
+        ],
     ],
 
     /*

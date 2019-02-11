d4381cef0b69628319d31113c66dedbcec6f0aac
vagrant <vagrant@homestead>
2019-02-11 09:03:23 +0000

    Admin can manage site settings

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -5,7 +5,7 @@
         </p>
 
         <p class="pull-right">
-            <a href="mailto:name@email.com">联系我们</a>
+            <a href="mailto:{{ setting('contact_email') }}">联系我们</a>
         </p>
     </div>
 </footer>

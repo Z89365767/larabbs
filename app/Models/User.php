423acd884c98f7f8577f0331d5fd4d6632ae677b
vagrant <vagrant@homestead>
2019-02-26 07:56:27 +0000

    活跃用户

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,12 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Auth;
 use Spatie\Permission\Traits\HasRoles;
+use App\Models\Traits;
 
 class User extends Authenticatable
 {
     use HasRoles;
+    use Traits\ActiveUserHelper;
 
 //默认的 User 模型中使用了 trait —— Notifiable，它包含着一个可以用来发通知的方法 notify() ，此方法接收一个通知实例做参数。虽然 notify() 已经很方便，但是我们还需要对其进行定制，我们希望每一次在调用 $user->notify() 时，自动将 users 表里的 notification_count +1 ，这样我们就能跟踪用户未读通知了。
     use Notifiable{
